9ee825e850cbfdc4088135f5e429097b4ff6e9b3
Roni Dover <roni.dover@gmail.com>
2016-07-22 16:34:36 +0300

    examples

---
+++ driver_deep_dive/src/driver.py
@@ -0,0 +1,26 @@
+from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
+from cloudshell.shell.core.context import InitCommandContext, ResourceCommandContext
+
+
+class DriverDeepDiveDriver (ResourceDriverInterface):
+
+    def cleanup(self):
+        """
+        Destroy the driver session, this function is called everytime a driver instance is destroyed
+        This is a good place to close any open sessions, finish writing to log files
+        """
+        pass
+
+    def __init__(self):
+        """
+        ctor must be without arguments, it is created with reflection at run time
+        """
+        pass
+
+    def initialize(self, context):
+        """
+        Initialize the driver session, this function is called everytime a new instance of the driver is created
+        This is a good place to load and cache the driver configuration, initiate sessions etc.
+        :param InitCommandContext context: the context the command runs on
+        """
+        pass
