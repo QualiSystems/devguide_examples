d2925bd73be0a2830662bc3a115d255988ad0020
Roni Dover <roni.dover@gmail.com>
2016-08-21 21:54:44 +0300

    udpated sample

--- devops_integration/sandbox_api_python_2_and_3/sandbox_api_example.py
+++ devops_integration/sandbox_api_python_2_and_3/sandbox_api_example.py
@@ -1,7 +1,15 @@
 import datetime
+import pprint
+import time
 
 from devops_integration.sandbox_api_python_2_and_3.sandbox_api.sandbox_apis import SandboxRESTAPI
 
+
+def run_some_tests_or_other_code(sandbox):
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(sandbox)
+
+    time.sleep(10)
 
 def main():
     """
@@ -11,7 +19,8 @@ def main():
     api_example = SandboxRESTAPI('localhost', '8032')
     api_example.login("admin", "admin", "Global")
     sandbox_id = api_example.start_sandbox('Simple blueprint', datetime.timedelta(hours=2), 'test_sandbox')
-    api_example.wait_for_sandbox_setup(sandbox_id)
+    sandox_details = api_example.wait_for_sandbox_setup(sandbox_id)
+    run_some_tests_or_other_code(sandox_details)
     api_example.stop_sandbox(sandbox_id)
     api_example.wait_for_sandbox_teardown(sandbox_id)
 
